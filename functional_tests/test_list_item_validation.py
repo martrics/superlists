7c779060b3d796fb44506006e049c9e09ae3794c
matrics <luminghi@hotmail.com>
2016-02-11 22:50:39 +0800

    Using ExistingListItemForm and finished chapter 12

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -30,4 +30,4 @@ class ItemValidationTest(FunctionalTest):
         
         self.check_for_row_in_list_table('1: Buy wellies')
         error = self.browser.find_element_by_css_selector('.has-error')
-        self.assertEqual(error.text, "You've already got this in you list")
+        self.assertEqual(error.text, "You've already got this in your list")
